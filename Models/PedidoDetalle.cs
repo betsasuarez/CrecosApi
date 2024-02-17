f0858aa47e06e845141b959fb8feeab688d59837
CORREA BETSABE <betsabe.correa@tecnologicoliceocristiano.edu.ec>
2024-02-16 22:31:51 -0500

    Agregar archivos de proyecto.

---
+++ Models/PedidoDetalle.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrecosApi.Models
+{
+    public class PedidoDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Pedido))]
+        public int PedidoId { get; set; }
+
+        [ForeignKey(nameof(Producto))]
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Costo { get; set; }
+    }
+}
